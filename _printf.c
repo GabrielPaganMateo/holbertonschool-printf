f9f7e909923aae6f9ffb2f6913c4cb2451ac75e9
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-08 18:35:29 +0000

    Added null string, % and % % edge cases.

--- _printf.c
+++ _printf.c
@@ -11,13 +11,20 @@ int _printf(const char *format, ...)
 	int i = 0;
 	int char_count = 0;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(a, format);
 
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			i++;
+			while (format[++i] == ' ')
+			{
+				/* spaces between the '%' and the format char are ignored. */
+			}
+
 			switch (format[i])
 			{
 				case 'c':
@@ -28,26 +35,32 @@ int _printf(const char *format, ...)
 				char_count += print_string(va_arg(a, char*));
 				break;
 
-				case 'i':
+				case 'd': case 'i':
 				char_count += print_int(va_arg(a, int));
 				break;
-
-				case 'd':
-				char_count += print_int(va_arg(a, int));
 				/*
-				 * for now, we're using decimal print_int for both
-				 * "%i" and "%d".
-				 */
+				* for now, we're using decimal print_int for both
+				* "%i" and "%d".
+				*/
+
+				case '%':
+				write(1, "%", 1);
+				char_count++;
 				break;
+				/* '%%' just cancels into '%' */
+
+				default:
+				return (-1);
 			}
 		}
 		else
 		{
 			write(1, &format[i], 1);
-			char_count += 1;
+			char_count++;
 		}
-	i++;
+
+		i++;
 	}
-va_end(a);
-return (char_count);
+	va_end(a);
+	return (char_count);
 }
